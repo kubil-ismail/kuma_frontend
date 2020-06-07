f4f4d450a390be7a26980bab110dddb143e1d242
Bilkis Ismail <bilkisismail07@gmail.com>
2020-06-07 12:42:35 +0700

    fix navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import store from 'store2'
 
 // Pages
 import Home from './pages/Home'
+import Detail from './pages/Detail'
+import Books from './pages/Books'
 import Login from './pages/Login'
 import Forget from './pages/Forget'
 import NotFound from './pages/NotFound'
-import SignUp from './pages/SignUp';
-import Detail from './pages/Detail';
+import SignUp from './pages/SignUp'
 
 function App() {
   const hasLogin = store('login')
@@ -16,6 +17,7 @@ function App() {
     <Router>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/books' component={Books} />
         <Route path='/detail' component={Detail} />
 
         {/* Auth */}
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -20,45 +20,52 @@ export default class Navbars extends Component {
   render() {
     const { hasLogin } = this.state
     return (
-      <Navbar bg="white" expand="lg" fixed="top" className="shadow-sm animate__animated animate__fadeInDown">
-        <Container>
-          <Link className="navbar-brand" to="/">
-            <img src={logo} width="100" alt="logo" />
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto ml-lg-3 text-center">
-              <Link className="nav-link" to="/">Home</Link>
-              <Link className="nav-link" to="/book">Book</Link>
-            </Nav>
-            <Nav className="ml-auto">
-              <Form inline>
-                <FormControl type="text" placeholder="Search book..." className="mr-sm-2 w-100" />
-              </Form>
-              {hasLogin
-                ? (
-                  <Fragment>
-                    <Link className="nav-link" to="/sign-up">
-                      <Button className="w-100">Profile</Button>
-                    </Link>
-                    <Nav.Link>
-                      <Button className="w-100" variant="outline-dark" onClick={(e) => this.logout()}>Logout</Button>
-                    </Nav.Link>
-                  </Fragment>
-                ) : (
-                  <Fragment>
-                    <Link className="nav-link" to="/sign-up">
-                      <Button className="w-100">Sign Up</Button>
-                    </Link>
-                    <Link className="nav-link" to="/login">
-                      <Button className="w-100" variant="outline-dark">Login</Button>
-                    </Link>
-                  </Fragment>
-                )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Fragment>
+        <Navbar bg="white" expand="lg" fixed="top" className="shadow-sm animate__animated animate__fadeInDown">
+          <Container>
+            <Link className="navbar-brand" to="/">
+              <img src={logo} width="100" alt="logo" />
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto ml-lg-3 text-center">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/book">Book</Link>
+              </Nav>
+              <Nav className="ml-auto">
+                <Form className="d-block-lg d-none" inline>
+                  <FormControl type="text" placeholder="Search book..." className="mr-sm-2 w-100" />
+                </Form>
+                {hasLogin
+                  ? (
+                    <Fragment>
+                      <Link className="nav-link" to="/sign-up">
+                        <Button className="w-100">Profile</Button>
+                      </Link>
+                      <Nav.Link>
+                        <Button className="w-100" variant="outline-dark" onClick={(e) => this.logout()}>Logout</Button>
+                      </Nav.Link>
+                    </Fragment>
+                  ) : (
+                    <Fragment>
+                      <Link className="nav-link" to="/sign-up">
+                        <Button className="w-100">Sign Up</Button>
+                      </Link>
+                      <Link className="nav-link" to="/login">
+                        <Button className="w-100" variant="outline-dark">Login</Button>
+                      </Link>
+                    </Fragment>
+                  )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+          <Form className="w-100 mt-4 mb-2 px-2 d-lg-none">
+            <FormControl type="text" placeholder="Search book..." className="mr-sm-2 w-100" />
+          </Form>
+        </Navbar>
+        <div className="nav-margin-md d-lg-none"></div>
+        <div className="nav-margin-lg d-lg-block d-none"></div>
+      </Fragment>
     )
   }
 }
