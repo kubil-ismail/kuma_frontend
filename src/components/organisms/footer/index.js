8aa6295b6781e1f686f81d5bd9a1f77a1b7e74c8
Bilkis Ismail <bilkisismail07@gmail.com>
2020-06-22 15:55:46 +0700

    fix footer

--- src/components/organisms/footer/index.js
+++ src/components/organisms/footer/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function index() {
   return (
-    <footer>
+    <footer id="sticky-footer">
       <div className="bottom-footer mt-4 py-1">
         <p className="text-white text-center mt-2">
-          Copyright © 2020 Kuma Book. Build By Bilkis Ismail
+          Copyright &copy; 2020 Kuma Book. Build By Bilkis Ismail
         </p>
       </div>
     </footer>
