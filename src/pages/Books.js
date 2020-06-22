43798b762de70357dafc4bffe03e7d4cc0c8b87e
Bilkis Ismail <bilkisismail07@gmail.com>
2020-06-22 14:42:36 +0700

    fetch all book

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-
-import cover from '../assets/img/cover.jpg';
+import { Container, Row, Col, Dropdown } from 'react-bootstrap';
+import Pagination from 'react-js-pagination';
+import { get } from '../services';
 
 // Component
 import Navbar from '../components/organisms/navbar';
@@ -16,12 +16,61 @@ export default class Books extends Component {
     super(props);
     this.state = {
       books: [],
+      options: [],
       error: false,
+      sort: 1,
     };
   }
 
+  getAllBooks = async () => {
+    try {
+      const books = await get({ url: 'book?limit=8' });
+      const { data, options } = books.data;
+      this.setState({
+        books: data,
+        options,
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  onSort = (sort) => {
+    const { books } = this.state;
+    if (parseInt(sort, 10) === 2) {
+      // DESC
+      books.sort((a, b) => {
+        return a - b;
+      });
+      this.setState({ books, sort: 2 });
+    } else {
+      // ASC
+      books.sort((a, b) => {
+        return a - b;
+      });
+      this.setState({ books, sort: 1 });
+    }
+  };
+
+  handlePageChange = async (page) => {
+    try {
+      const books = await get({ url: `book?limit=8&page=${page}` });
+      const { data, options } = books.data;
+      this.setState({
+        books: data,
+        options,
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  componentDidMount = () => {
+    this.getAllBooks();
+  };
+
   render() {
-    const { books, error } = this.state;
+    const { books, options, error, sort } = this.state;
     return (
       <>
         {/* Navbar */}
@@ -31,9 +80,24 @@ export default class Books extends Component {
         {/* Popular Book */}
         <section>
           <Container className="my-5">
-            <div className="head-title">
-              <h3 className="main-title font-weight-bold">List Book</h3>
-              <div className="divinder" />
+            <div className="d-flex justify-content-between">
+              <div className="head-title">
+                <h3 className="main-title font-weight-bold">List Book</h3>
+                <div className="divinder w-50" />
+              </div>
+              <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  Sort
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item active={sort === 1} onClick={() => this.onSort('1')}>
+                    Sort by name A - Z
+                  </Dropdown.Item>
+                  <Dropdown.Item active={sort === 2} onClick={() => this.onSort('2')}>
+                    Sort by name Z - A
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
 
             {/* Show if failed fetch */}
@@ -43,44 +107,32 @@ export default class Books extends Component {
             {!books.length ? (
               <BookLoader />
             ) : (
-              <Row>
-                <Col lg={3} md={6} xs={6}>
-                  <Book
-                    cover={cover}
-                    title="Overlord"
-                    author="Maruyama Sensei"
-                    genre="Horror"
-                    languag="Japan"
-                  />
-                </Col>
-                <Col lg={3} md={6} xs={6}>
-                  <Book
-                    cover={cover}
-                    title="Overlord"
-                    author="Maruyama Sensei"
-                    genre="Horror"
-                    languag="Japan"
-                  />
-                </Col>
-                <Col lg={3} md={6} xs={6}>
-                  <Book
-                    cover={cover}
-                    title="Overlord"
-                    author="Maruyama Sensei"
-                    genre="Horror"
-                    languag="Japan"
-                  />
-                </Col>
-                <Col lg={3} md={6} xs={6}>
-                  <Book
-                    cover={cover}
-                    title="Overlord"
-                    author="Maruyama Sensei"
-                    genre="Horror"
-                    languag="Japan"
-                  />
-                </Col>
-              </Row>
+              <>
+                <Row>
+                  {books.map((val) => (
+                    <Col className="mb-5" lg={3} md={6} xs={6} key={val.id}>
+                      <Book
+                        id={val.id}
+                        cover={val.cover}
+                        title={val.name}
+                        author={val.author}
+                        genre={val.genre}
+                        language={val.language}
+                      />
+                    </Col>
+                  ))}
+                </Row>
+                <Pagination
+                  activePage={options.page}
+                  itemsCountPerPage={options.perPage}
+                  totalItemsCount={parseInt(options.totalData, 10)}
+                  pageRangeDisplayed={5}
+                  onChange={this.handlePageChange}
+                  itemClass="page-item"
+                  linkClass="page-link"
+                  hideNavigation
+                />
+              </>
             )}
           </Container>
         </section>
